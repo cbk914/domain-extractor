01215808e334404405982e2626f044309029ae99
cbk914 <cbk914@riseup.net>
2023-03-18 20:13:25 +0100

    Update

--- domain-extractor.py
+++ domain-extractor.py
@@ -24,6 +24,12 @@ def extract_domains_from_url(url):
     domain_pattern = r'(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+'
     domain_list = re.findall(domain_pattern, content)
 
+    # If no domains are found in the content, check the URL domain itself
+    if not domain_list:
+        url_domain = re.match(domain_pattern, url)
+        if url_domain:
+            domain_list.append(url_domain.group(0))
+
     return domain_list
 
 def extract_domains_from_file(file_path):
@@ -86,15 +92,16 @@ def extract_domains_from_zone_file(file_path):
     return domain_list
 
 def check_domains(file_path):
+    checked_domains = []
+
     try:
         with open(file_path, 'r') as file:
             domain_list = file.readlines()
     except FileNotFoundError:
         print(f"Error: File not found - {file_path}")
-        return
+        return checked_domains
 
     start_time = time.time()
-
     for url in domain_list:
         url = url.strip()
         if not url.startswith("http://") and not url.startswith("https://"):
@@ -103,15 +110,26 @@ def check_domains(file_path):
             response = requests.get(url, timeout=10)
             status_code = response.status_code
             if 100 <= status_code < 400:
+                checked_domains.append((url, status_code, []))
                 print(f"[{status_code}] {url}")
                 if response.history:
                     for res in response.history:
                         print(f"\tRedirected To: [Response:{res.status_code}] {res.url}")
+                        checked_domains[-1][2].append((res.url, res.status_code))
                     print(f"\tFinal Redirection: [Response:{response.status_code}] {response.url}")
         except (requests.exceptions.RequestException, IOError):
             pass
 
     print(f"\n[!] Finished in {int(time.time() - start_time)} second(s).")
+    return checked_domains
+
+def save_checked_domains_to_file(checked_domains, output_file):
+    with open(output_file, 'w') as file:
+        for url, status_code, redirects in checked_domains:
+            file.write(f"[{status_code}] {url}\n")
+            if redirects:
+                for redirect_url, redirect_status_code in redirects:
+                    file.write(f"\tRedirected To: [Response:{redirect_status_code}] {redirect_url}\n")
 
 def extract_domains_from_text_file(file_path):
     try:
@@ -142,12 +160,11 @@ def resolve_ip_to_domain(ip_address):
 
 def main():
     parser = argparse.ArgumentParser(description="Extract domain names from a URL, file, or zone file and check the domains.")
-    parser.add_argument('-u', '--url', help='URL containing domain names')
-    parser.add_argument('-f', '--file', help='File containing domain names (txt, html, csv, json, xml)')
-    parser.add_argument('-z', '--zone', help='Zone file containing domain names')
+    parser.add_argument('-u', '--url', action='append', help='URL containing domain names')
+    parser.add_argument('-f', '--file', action='append', help='File containing domain names (txt, html, csv, json, xml)')
+    parser.add_argument('-z', '--zone', action='append', help='Zone file containing domain names')
     parser.add_argument('-c', '--check', help='Check domains in the provided file')
     parser.add_argument('-o', '--output', default='domains.txt', help='Output file to save the extracted domain names')
-
     args = parser.parse_args()
 
     if not any([args.url, args.file, args.zone, args.check]):
@@ -155,32 +172,32 @@ def main():
         parser.error("Error: At least one of the following arguments is required: -u/--url, -f/--file, -z/--zone, -c/--check")
 
     if args.check:
-        check_domains_thread = threading.Thread(target=check_domains, args=(args.check,))
-        check_domains_thread.start()
-        check_domains_thread.join()
-        return
+        checked_domains = check_domains(args.check)
+        save_checked_domains_to_file(checked_domains, 'domains_checked.txt')
 
     all_domains = []
-
     if args.url:
-        domains_from_url = extract_domains_from_url(args.url)
-        all_domains.extend(domains_from_url)
+        for url in args.url:
+            domains_from_url = extract_domains_from_url(url)
+            all_domains.extend(domains_from_url)
 
     if args.file:
-        domains_from_file = extract_domains_from_file(args.file)
-        all_domains.extend(domains_from_file)
+        for file_path in args.file:
+            domains_from_file = extract_domains_from_file(file_path)
+            all_domains.extend(domains_from_file)
 
     if args.zone:
-        domains_from_zone_file = extract_domains_from_zone_file(args.zone)
-        all_domains.extend(domains_from_zone_file)
+        for zone_file in args.zone:
+            domains_from_zone_file = extract_domains_from_zone_file(zone_file)
+            all_domains.extend(domains_from_zone_file)
 
     unique_domains = list(set(all_domains))
     save_domains_to_file(unique_domains, args.output)
 
     ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
     ip_addresses = list(set(re.findall(ip_pattern, ' '.join(unique_domains))))
+
     resolved_domains = []
-    
     for ip in ip_addresses:
         domain = resolve_ip_to_domain(ip)
         if domain:
